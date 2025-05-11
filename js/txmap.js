e531bd1916faf1f7d7421b11f9ab483873b469ad
shigu <2297726597@qq.com>
2025-05-11 18:31:48 +0800

    Site updated: 2025-05-11 18:31:48

--- js/txmap.js
+++ js/txmap.js
@@ -3,7 +3,7 @@ $.ajax({
     type: 'get',
     url: 'https://apis.map.qq.com/ws/location/v1/ip',
     data: {
-        key: 'CWABZ-CJRLJ-SKQF6-DHFDI-D2GDS-C4FMK',
+        key: 'DCZBZ-T4UY5-55QII-IJXJH-W67ZV-IYBUB',
         output: 'jsonp',
     },
     dataType: 'jsonp',
@@ -215,7 +215,7 @@ function showWelcome() {
     try {
         //自定义文本和需要放的位置
         document.getElementById("welcome-info").innerHTML =
-            `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
+            `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里.今天也要开心呢</span> ${posdesc}</b>`;
     } catch (err) {
         // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
     }
